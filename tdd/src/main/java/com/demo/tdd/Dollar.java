4c0b3959a5bea8cd11e67c51a46646c55d12c628
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-07-15 23:31:40 +0630

    day four

--- tdd/src/main/java/com/demo/tdd/Dollar.java
+++ tdd/src/main/java/com/demo/tdd/Dollar.java
@@ -1,14 +1,14 @@
-package com.demo.tdd;
-
-public class Dollar extends Money {
-
-	public Dollar(int amount,String currency) {
-		super(amount,currency);
-	} 
-	   
-	@Override
-	public String currency() { 
-		return this.currency;
-	}
- 
-}
+//package com.demo.tdd;
+//
+//public class Dollar extends Money {
+//
+//	public Dollar(int amount,String currency) {
+//		super(amount,currency);
+//	} 
+//	   
+//	@Override
+//	public String currency() { 
+//		return this.currency;
+//	}
+// 
+//}
--- tdd/src/main/java/com/demo/tdd/Franc.java
+++ tdd/src/main/java/com/demo/tdd/Franc.java
@@ -1,14 +1,14 @@
-package com.demo.tdd;
-
-public class Franc extends Money{
- 
-	public Franc(int amount,String currency) {
-		super(amount, currency);
-	} 
-	
-	@Override
-	public String currency() { 
-		return this.currency;
-	} 
-	
-}
+//package com.demo.tdd;
+//
+//public class Franc extends Money{
+// 
+//	public Franc(int amount,String currency) {
+//		super(amount, currency);
+//	} 
+//	
+//	@Override
+//	public String currency() { 
+//		return this.currency;
+//	} 
+//	
+//}
